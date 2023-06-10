1ac7da06bed147395080cca6fff180dc18944527
jisung Han <jishan.han@mail.utoronto.ca>
2023-06-09 20:57:36 -0400

    test1

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -128,13 +128,7 @@ public class Silly implements Comparable<Silly>{
         x.countStatic();
 
        // int[] expected_values = {x.countStatic(), y.countStatic(), x.countStatic(), x.countStatic()};
-        int[] expected_values ={
-                expected_values[0] = x.countStatic(),
-                expected_values[1] = y.countStatic(),
-                expected_values[2] = x.countStatic(),
-                expected_values[3] = x.countStatic(),
-
-       };
+        int[] expected_values ={expected_values[0] = x.countStatic(), expected_values[1] = y.countStatic(), expected_values[2] = x.countStatic(), expected_values[3] = x.countStatic(),};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
